4083af38fbddb1a83402916633c080cbb3c1cc11
sn4ke-laptop <michael.retamozo@gmail.com>
2022-07-07 23:36:34 +0200

    update: pruebas... 🤔

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,67 +1,80 @@
 import React from "react";
 import {
-    Container,
-    Title,
-    Description,
-    ScrollDown,
-    HtmlBody,
-    IconAnimated,
-    LetraAnimada,
-    NombreAnimado,
-    WebAnimado,
+  Container,
+  Title,
+  Description,
+  ScrollDown,
+  HtmlBody,
+  IconAnimated,
+  LetraAnimada,
+  NombreAnimado,
+  WebAnimado,
+  Section,
 } from "./components/Home.styled";
 import SvgComponent from "./components/SvgComponent";
 
 export const Home = () => {
-    return (
-        <>
-            <Container
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
+  return (
+    <>
+      <Container
+        id="style-1"
+      >
+        <Section
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <IconAnimated>
+            <a
+              href="https://github.com/snakeblack"
+              rel="noreferrer"
+              target="_blank"
             >
-                <IconAnimated>
-                    <a
-                        href="https://github.com/snakeblack"
-                        rel="noreferrer"
-                        target="_blank"
-                    >
-                        <SvgComponent />
-                    </a>
-                </IconAnimated>
-                <HtmlBody>
-                    &lt;html&gt;
-                    <br />
-                    &lt;body&gt;
-                </HtmlBody>
-                <Title>
-                    <br />
-                    <LetraAnimada>H</LetraAnimada>
-                    <LetraAnimada>i</LetraAnimada>
-                    <LetraAnimada>,</LetraAnimada>
-                    <br />
-                    <LetraAnimada>I</LetraAnimada>
-                    <LetraAnimada>'</LetraAnimada>
-                    <LetraAnimada>m</LetraAnimada>
-                    &nbsp;
-                    <NombreAnimado>M</NombreAnimado>
-                    <NombreAnimado>a</NombreAnimado>
-                    <NombreAnimado>n</NombreAnimado>
-                    <NombreAnimado>u</NombreAnimado>
-                    <NombreAnimado>e</NombreAnimado>
-                    <NombreAnimado>l</NombreAnimado>
-                    <LetraAnimada>,</LetraAnimada>
-                    <br />
-                    <WebAnimado>Web</WebAnimado>&nbsp;
-                    <WebAnimado>Developer</WebAnimado>
-                </Title>
-                <Description>
-                    <br />
-                    Full&nbsp;Stack&nbsp;Developer&nbsp;/&nbsp;MERN&nbsp;&&nbsp;JAVA
-                    <br />
-                </Description>
-                <ScrollDown />
-            </Container>
-        </>
-    );
+              <SvgComponent />
+            </a>
+          </IconAnimated>
+          <HtmlBody>
+            &lt;html&gt;
+            <br />
+            &lt;body&gt;
+          </HtmlBody>
+          <Title>
+            <br />
+            <LetraAnimada>H</LetraAnimada>
+            <LetraAnimada>i</LetraAnimada>
+            <LetraAnimada>,</LetraAnimada>
+            <br />
+            <LetraAnimada>I</LetraAnimada>
+            <LetraAnimada>'</LetraAnimada>
+            <LetraAnimada>m</LetraAnimada>
+            &nbsp;
+            <NombreAnimado>M</NombreAnimado>
+            <NombreAnimado>a</NombreAnimado>
+            <NombreAnimado>n</NombreAnimado>
+            <NombreAnimado>u</NombreAnimado>
+            <NombreAnimado>e</NombreAnimado>
+            <NombreAnimado>l</NombreAnimado>
+            <LetraAnimada>,</LetraAnimada>
+            <br />
+            <WebAnimado>Web</WebAnimado>&nbsp;
+            <WebAnimado>Developer</WebAnimado>
+          </Title>
+          <Description>
+            <br />
+            Full&nbsp;Stack&nbsp;Developer&nbsp;/&nbsp;MERN&nbsp;&&nbsp;JAVA
+            <br />
+          </Description>
+        </Section>
+        <ScrollDown>
+            <a href="#section-2"></a>
+        </ScrollDown>
+        <Section
+            id="section-2"
+        >
+            <Title>TEXTO DE PRUEBA</Title>
+        </Section>
+        <ScrollDown />
+      </Container>
+    </>
+  );
 };
--- src/pages/home/components/Home.styled.js
+++ src/pages/home/components/Home.styled.js
@@ -2,249 +2,301 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 export const HtmlBody = styled.span`
+  color: #1a4a4d;
+  font-family: "Island Moments", cursive;
+  font-size: 1.8vw;
+  font-weight: 700;
+  @media screen and (max-width: 500px) {
+    font-size: 16px;
+  }
+
+  h1::after {
+    content: "<html>";
     color: #1a4a4d;
     font-family: "Island Moments", cursive;
     font-size: 1.8vw;
     font-weight: 700;
     @media screen and (max-width: 500px) {
-        font-size: 16px;
+      font-size: 16px;
     }
-
-    h1::after {
-        content: "<html>";
-        color: #1a4a4d;
-        font-family: "Island Moments", cursive;
-        font-size: 1.8vw;
-        font-weight: 700;
-        @media screen and (max-width: 500px) {
-            font-size: 16px;
-        }
-    }
+  }
 `;
 
 export const Container = styled(motion.main)`
-    display: flex;
-    flex-direction: column;
-    height: 90vh;
-    width: 80%;
-    margin: 0 auto;
-    justify-content: center;
+  height: 100vh;
+  width: 100%;
+  overflow-y: auto;
+`;
 
-    h1::before {
-        content: "<h1>";
-        color: #1a4a4d;
-        filter: grayscale(0);
-        font-family: "Island Moments", cursive;
-        font-size: 1.8vw;
-        font-weight: 700;
-        background: #1a4a4d;
-        -webkit-background-clip: text;
-        @media screen and (max-width: 500px) {
-            font-size: 16px;
-        }
-    }
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  height: 90vh;
+  width: 80vw;
+  margin: 0 auto;
+  justify-content: center;
 
-    h1::after {
-        content: " </h1>";
-        color: #1a4a4d;
-        filter: grayscale(0);
-        font-family: "Island Moments", cursive;
-        font-size: 1.8vw;
-        font-weight: 700;
-        background: #1a4a4d;
-        -webkit-background-clip: text;
-        @media screen and (max-width: 500px) {
-            font-size: 16px;
-        }
+  h1::before {
+    content: "<h1>";
+    color: #1a4a4d;
+    filter: grayscale(0);
+    font-family: "Island Moments", cursive;
+    font-size: 1.8vw;
+    font-weight: 700;
+    background: #1a4a4d;
+    -webkit-background-clip: text;
+    @media screen and (max-width: 500px) {
+      font-size: 16px;
     }
+  }
 
-    p::before {
-        content: "<p>";
-        color: #1a4a4d;
-        font-family: "Island Moments", cursive;
-        font-size: 1.8vw;
-        font-weight: 700;
-        @media screen and (max-width: 500px) {
-            font-size: 16px;
-        }
+  h1::after {
+    content: " </h1>";
+    color: #1a4a4d;
+    filter: grayscale(0);
+    font-family: "Island Moments", cursive;
+    font-size: 1.8vw;
+    font-weight: 700;
+    background: #1a4a4d;
+    -webkit-background-clip: text;
+    @media screen and (max-width: 500px) {
+      font-size: 16px;
     }
+  }
 
-    p::after {
-        content: " </p>";
-        color: #1a4a4d;
-        font-family: "Island Moments", cursive;
-        font-size: 1.8vw;
-        font-weight: 700;
-        @media screen and (max-width: 500px) {
-            font-size: 16px;
-        }
+  p::before {
+    content: "<p>";
+    color: #1a4a4d;
+    font-family: "Island Moments", cursive;
+    font-size: 1.8vw;
+    font-weight: 700;
+    @media screen and (max-width: 500px) {
+      font-size: 16px;
     }
+  }
 
-    @media (max-width: 500px) {
-        transform: scale(1.15);
+  p::after {
+    content: " </p>";
+    color: #1a4a4d;
+    font-family: "Island Moments", cursive;
+    font-size: 1.8vw;
+    font-weight: 700;
+    @media screen and (max-width: 500px) {
+      font-size: 16px;
     }
+  }
+
+  @media (max-width: 500px) {
+    transform: scale(1.15);
+  }
 `;
 
 export const Title = styled.h1`
-    color: var(--silver);
-    font-family: "Manrope", sans-serif;
-    font-size: 6vw;
-    font-weight: 600;
-    justify-content: center;
-    margin: 0 0;
+  color: var(--silver);
+  font-family: "Manrope", sans-serif;
+  font-size: 6vw;
+  font-weight: 600;
+  justify-content: center;
+  margin: 0 0;
 
-    @media screen and (max-width: 1024px) {
-        font-size: 5.2rem;
-    }
+  @media screen and (max-width: 1024px) {
+    font-size: 5.2rem;
+  }
 
-    @media screen and (max-width: 960px) {
-        font-size: 5rem;
-    }
+  @media screen and (max-width: 960px) {
+    font-size: 5rem;
+  }
 
-    @media screen and (max-width: 818px) {
-        font-size: 3.7rem;
-    }
+  @media screen and (max-width: 818px) {
+    font-size: 3.7rem;
+  }
 
-    @media screen and (max-width: 618px) {
-        font-size: 3.1rem;
-    }
+  @media screen and (max-width: 618px) {
+    font-size: 3.1rem;
+  }
 
-    @media screen and (max-width: 530px) {
-        font-size: 3rem;
-    }
+  @media screen and (max-width: 530px) {
+    font-size: 3rem;
+  }
 
-    @media screen and (max-width: 432px) {
-        font-size: 2.7rem;
-    }
+  @media screen and (max-width: 432px) {
+    font-size: 2.7rem;
+  }
 
-    @media screen and (max-width: 394px) {
-        font-size: 2.4rem;
-    }
+  @media screen and (max-width: 394px) {
+    font-size: 2.4rem;
+  }
 
-    @media screen and (max-width: 375px) {
-        font-size: 2.2rem;
-    }
+  @media screen and (max-width: 375px) {
+    font-size: 2.2rem;
+  }
 `;
 
 export const NombreAnimado = styled.span`
-    display: inline-block;
-    font-weight: 800;
-    cursor: url('https://firebasestorage.googleapis.com/v0/b/portfolio-581ed.appspot.com/o/images%2Fcursor.png?alt=media&token=efe8c296-c259-4e12-baf7-29311013ec0e'), auto;
+  display: inline-block;
+  font-weight: 800;
+  cursor: url("https://firebasestorage.googleapis.com/v0/b/portfolio-581ed.appspot.com/o/images%2Fcursor.png?alt=media&token=efe8c296-c259-4e12-baf7-29311013ec0e"),
+    auto;
 
-    -webkit-transition: all 0.3s ease-out;
-    -o-transition: all 0.3s ease-out;
-    transition: all 0.3s ease-out;
-    color: var(--bright-maroon);
+  -webkit-transition: all 0.3s ease-out;
+  -o-transition: all 0.3s ease-out;
+  transition: all 0.3s ease-out;
+  color: var(--bright-maroon);
 
-    &:hover {
-        color: #B2ECE1;
-        -webkit-animation-name: rubberBand;
-        animation-name: rubberBand;
-        -webkit-animation-duration: 1s;
-        animation-duration: 1s;
-        -webkit-animation-fill-mode: both;
-        animation-fill-mode: both;
-    }
+  &:hover {
+    color: #b2ece1;
+    -webkit-animation-name: rubberBand;
+    animation-name: rubberBand;
+    -webkit-animation-duration: 1s;
+    animation-duration: 1s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+  }
 `;
 
 export const LetraAnimada = styled.span`
-    display: inline-block;
-    font-weight: 800;
-    cursor: url('https://firebasestorage.googleapis.com/v0/b/portfolio-581ed.appspot.com/o/images%2Fcursor.png?alt=media&token=efe8c296-c259-4e12-baf7-29311013ec0e'), auto;
+  display: inline-block;
+  font-weight: 800;
+  cursor: url("https://firebasestorage.googleapis.com/v0/b/portfolio-581ed.appspot.com/o/images%2Fcursor.png?alt=media&token=efe8c296-c259-4e12-baf7-29311013ec0e"),
+    auto;
 
-    -webkit-transition: all 0.3s ease-out;
-    -o-transition: all 0.3s ease-out;
-    transition: all 0.3s ease-out;
+  -webkit-transition: all 0.3s ease-out;
+  -o-transition: all 0.3s ease-out;
+  transition: all 0.3s ease-out;
 
-    &:hover {
-        color: var(--metallic-seaweed);
-        -webkit-animation-name: rubberBand;
-        animation-name: rubberBand;
-        -webkit-animation-duration: 1s;
-        animation-duration: 1s;
-        -webkit-animation-fill-mode: both;
-        animation-fill-mode: both;
-    }
+  &:hover {
+    color: var(--metallic-seaweed);
+    -webkit-animation-name: rubberBand;
+    animation-name: rubberBand;
+    -webkit-animation-duration: 1s;
+    animation-duration: 1s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+  }
 `;
 
 export const WebAnimado = styled.span`
-    display: inline-block;
-    font-weight: 800;
-    cursor: url('https://firebasestorage.googleapis.com/v0/b/portfolio-581ed.appspot.com/o/images%2Fcursor.png?alt=media&token=efe8c296-c259-4e12-baf7-29311013ec0e'), auto;
+  display: inline-block;
+  font-weight: 800;
+  cursor: url("https://firebasestorage.googleapis.com/v0/b/portfolio-581ed.appspot.com/o/images%2Fcursor.png?alt=media&token=efe8c296-c259-4e12-baf7-29311013ec0e"),
+    auto;
 
-    -webkit-transition: all 0.3s ease-out;
-    -o-transition: all 0.3s ease-out;
-    transition: all 0.3s ease-out;
-    color: var(--silver);
-    &:hover {
-        color: var(--bright-maroon);
-        -webkit-animation: glitch 2500ms infinite linear alternate-reverse;
-        animation: glitch 2500ms infinite linear alternate-reverse;
-    }
+  -webkit-transition: all 0.3s ease-out;
+  -o-transition: all 0.3s ease-out;
+  transition: all 0.3s ease-out;
+  color: var(--silver);
+  &:hover {
+    color: var(--bright-maroon);
+    -webkit-animation: glitch 2500ms infinite linear alternate-reverse;
+    animation: glitch 2500ms infinite linear alternate-reverse;
+  }
 `;
 
-
 export const Description = styled.p`
-    align-items: center;
-    color: #c7abab;
-    font-family: "Manrope", sans-serif;
-    font-size: 1.3vw;
-    font-weight: 200;
-    justify-content: center;
-    margin: 0;
+  align-items: center;
+  color: #c7abab;
+  font-family: "Manrope", sans-serif;
+  font-size: 1.3vw;
+  font-weight: 200;
+  justify-content: center;
+  margin: 0;
 
-    @media screen and (max-width: 1024px) {
-        font-size: 1.6rem;
-    }
+  @media screen and (max-width: 1024px) {
+    font-size: 1.6rem;
+  }
 
-    @media screen and (max-width: 818px) {
-        font-size: 1.5rem;
-    }
+  @media screen and (max-width: 818px) {
+    font-size: 1.5rem;
+  }
 
-    @media screen and (max-width: 618px) {
-        font-size: 1.4rem;
-    }
+  @media screen and (max-width: 618px) {
+    font-size: 1.4rem;
+  }
 
-    @media screen and (max-width: 530px) {
-        font-size: 1.3rem;
-    }
+  @media screen and (max-width: 530px) {
+    font-size: 1.3rem;
+  }
 
-    @media screen and (max-width: 432px) {
-        font-size: 1.1rem;
-    }
+  @media screen and (max-width: 432px) {
+    font-size: 1.1rem;
+  }
 
-    @media screen and (max-width: 394px) {
-        font-size: 1rem;
-    }
+  @media screen and (max-width: 394px) {
+    font-size: 1rem;
+  }
 
-    @media screen and (max-width: 375px) {
-        font-size: 0.8rem;
-    }
+  @media screen and (max-width: 375px) {
+    font-size: 0.8rem;
+  }
 `;
 
 export const IconAnimated = styled.div`
-    position: absolute;
-    top: 5%;
-    right: 4%;
-    width: 20vw;
-    height: auto;
-    cursor: url('https://firebasestorage.googleapis.com/v0/b/portfolio-581ed.appspot.com/o/images%2Fpointer.png?alt=media&token=02d26594-9d74-4d6c-b0c2-e3c63a558ef8'), pointer;
-    
-    svg {
-        fill: var(--silver);
-        filter:drop-shadow(10px 4px 20px #000);
-        transition: all .5s ease-in-out .1s;
-        transform: rotate(12deg);
-        &:hover {
-            transition: all .5s ease-in-out 0s;
-            fill: var(--bright-maroon);
-            transform: rotate(-12deg) scale(1.3);
-        }   
-    } 
+  position: absolute;
+  top: 5%;
+  right: 4%;
+  width: 20vw;
+  height: auto;
+  cursor: url("https://firebasestorage.googleapis.com/v0/b/portfolio-581ed.appspot.com/o/images%2Fpointer.png?alt=media&token=02d26594-9d74-4d6c-b0c2-e3c63a558ef8"),
+    pointer;
 
-    @media (max-width: 900px) {
-        display: none;
+  svg {
+    fill: var(--silver);
+    filter: drop-shadow(10px 4px 20px #000);
+    transition: all 0.5s ease-in-out 0.1s;
+    transform: rotate(12deg);
+    &:hover {
+      transition: all 0.5s ease-in-out 0s;
+      fill: var(--bright-maroon);
+      transform: rotate(-12deg) scale(1.3);
     }
+  }
+
+  @media (max-width: 900px) {
+    display: none;
+  }
 `;
 
-export const ScrollDown = styled.a``;
+export const ScrollDown = styled.div`
+  display: flex;
+  width: 100%;
+  height: 10vh;
+  text-align: center;
+  justify-content: end;
+  padding-right: 2rem;
+
+  a {
+    width: 60px;
+    height: 60px;
+    border: 2px solid #333;
+    border-radius: 50%;
+    position: relative;
+    animation: down 1.5s infinite;
+    -webkit-animation: down 1.5s infinite;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 15px;
+      left: 18px;
+      width: 18px;
+      height: 18px;
+      border-left: 2px solid #333;
+      border-bottom: 2px solid #333;
+      transform: rotate(-45deg);
+    }
+  }
+
+  @media screen and (max-width: 618px) {
+    a {
+      width: 30px;
+      height: 30px;
+
+      &::before {
+        top: 7px;
+        left: 8px;
+        width: 8px;
+        height: 8px;
+      }
+    }
+  }
+`;
